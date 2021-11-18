fa82d755fe3711210738b6a4104746a4dbec7076
MONDY Cedric <cedric.mondy@ofb.gouv.fr>
2021-11-18 15:38:41 +0100

    data: préparation des données internes au package

---
+++ data-raw/preparation_donnees_internes.R
@@ -0,0 +1,47 @@
+library(BilanROE)
+
+Bassins <- NULL
+Departements <- c("75", "77", "78", "91", "92", "93", "94", "95")
+
+DonneesBrutes_1 <- importer_donnees(
+    chemin_archive = "dev/geobs_interne2020_09_22.zip",
+    bassins = Bassins,
+    departements = Departements
+)
+
+DonneesBrutes_2 <- importer_donnees(
+    chemin_archive = "dev/geobs_interne2021_03_13_sansRadars.zip",
+    bassins = Bassins,
+    departements = Departements
+)
+
+DonneesBrutes_3 <- importer_donnees(
+    chemin_archive = "dev/geobs_interne2021_06_20_sansRadars.zip",
+    bassins = Bassins,
+    departements = Departements
+)
+
+OuvragesPrioritaires <- readxl::read_xlsx(
+    path = "dev/200525_ouvrages_prioritaires.xlsx") %>%
+    dplyr::select(identifiant_roe = `Code ROE`) %>%
+    dplyr::mutate(prioritaire = TRUE)
+
+LimitesZone <- telecharger_limites_region(codes_region = "11")
+
+ReseauHydro <- telecharger_ce_topage(bassin = "03", limites_zone = LimitesZone)
+
+MassesEau <- telecharger_masses_eau(limites_zone = LimitesZone)
+
+Listes2 <- telecharger_listes2(limites_zone = LimitesZone)
+
+usethis::use_data(
+    DonneesBrutes_1, DonneesBrutes_2, DonneesBrutes_3,
+    OuvragesPrioritaires,
+    LimitesZone,
+    ReseauHydro,
+    MassesEau,
+    Listes2,
+    internal = TRUE,
+    overwrite = TRUE
+)
+
